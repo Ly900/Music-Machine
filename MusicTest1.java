631da5bb5532178abfca632fa7d0b69f8cc26dfb
Ly Nguyen <Ly900@aol.com>
2016-09-04 21:55:05 -0400

    Complete simple music machine app that plays one note.

--- MusicTest1.java
+++ MusicTest1.java
@@ -2,20 +2,38 @@ import javax.sound.midi.*;
 
 public class MusicTest1 {
 
+  public static void main(String[] args) {
+    MusicTest1 mini = new MusicTest1();
+    mini.play();
+  }
+
   public void play() {
 
     try {
-      Sequencer sequencer = MidiSystem.getSequencer();
-      System.out.println("We got a sequencer");
-    } catch (MidiUnavailableException ex) {
-      System.out.println("Bummer");
+      Sequencer player = MidiSystem.getSequencer();
+      player.open();
+
+      Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+      Track track = seq.createTrack();
+
+      ShortMessage a = new ShortMessage();
+      a.setMessage(144, 1, 44, 100);
+      MidiEvent noteOn = new MidiEvent(a,1);
+      track.add(noteOn);
+
+      ShortMessage b = new ShortMessage();
+      b.setMessage(128, 1, 44, 100);
+      MidiEvent noteOff = new MidiEvent(b, 16);
+      track.add(noteOff);
+
+      player.setSequence(seq);
+      player.start();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+
     }
-
-  }
-
-  public static void main(String[] args) {
-    MusicTest1 mt = new MusicTest1();
-    mt.play();
-  }
+  } // Ends play method
 
 }
